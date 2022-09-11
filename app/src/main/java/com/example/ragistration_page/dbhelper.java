66ce3367c643f337d3f23ddc808e2c6d3c7df7a4
Arnav Parmar <arnavparmar2000@gmail.com>
2022-09-11 13:03:34 +0530

    update-3

--- app/src/main/java/com/example/ragistration_page/dbhelper.java
+++ app/src/main/java/com/example/ragistration_page/dbhelper.java
@@ -2,6 +2,7 @@ package com.example.ragistration_page;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -22,15 +23,67 @@ public class dbhelper extends SQLiteOpenHelper {
     }
 
 
-    public boolean inputuserdata(String name, String passsword, String dob, String email, String phone, String state)
-    {
+    public boolean inputuserdata(String name, String passsword, String dob, String email, String phone, String state) {
         SQLiteDatabase DB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
-        contentValues.put("name", name);
-        contentValues.put("name", name);
-        contentValues.put("name", name);
-        contentValues.put("name", name);
-        contentValues.put("name", name);
+        contentValues.put("password", passsword);
+        contentValues.put("dob", dob);
+        contentValues.put("email", email);
+        contentValues.put("phone", phone);
+        contentValues.put("state", state);
+
+
+        long result = DB.insert("registration", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    public boolean updateuserdata(String name, String password, String dob, String email, String phone, String state) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("password", password);
+        contentValues.put("dob", dob);
+        contentValues.put("email", email);
+        contentValues.put("phone", phone);
+        contentValues.put("state", state);
+        Cursor cursor = DB.rawQuery("SELECT * FROM userdetails WHERE name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("registration", contentValues, "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public  boolean deleteuserdata(String name)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        Cursor cursor = DB.rawQuery("DELETE FROM registration where name = ?", new String[]{name});
+        if(cursor.getCount() == -1)
+        {
+            return  false;
+        }
+        else
+        {
+            return  true;
+        }
+    }
+
+    public Cursor getdata()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("SELECT * FROM registration", null);
+        return cursor;
     }
 }
